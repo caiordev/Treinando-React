50369970d15ea0e3f865fcaf401fe097dcbd53d3
caiordev <caioreisdev@gmail.com>
2023-09-04 15:07:02 -0300

    feat: criando componente para login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,22 +1,62 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { InputLogin } from "./components/InputLogin";
 
 export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
-  const handleEntrar = () => {
+  const emailLength = useMemo(() => {
+    return email.length;
+  }, [email.length]);
+
+  useEffect(() => {
+    if (window.confirm("Voce é homem?")) {
+      console.log("homem");
+    } else {
+      console.log("mulher");
+    }
+  }, []);
+
+  useEffect(() => {
     console.log(email);
     console.log(senha);
-  };
+  }, [email, senha]);
+
+  //assim que inicia ele não faz logo pq ela é chamada apenas com o click.
+  const handleEntrar = useCallback(() => {
+    console.log(email);
+    console.log(senha);
+  }, [email, senha]);
+
   return (
     <div>
       <form action="">
-        <label htmlFor="">
+        <p>Quantidade de caracteres do email: {emailLength}</p>
+
+        <InputLogin
+          label="Email"
+          value={email}
+          onChange={(newValue) => setEmail(newValue)}
+          onPressEnter={() => inputPasswordRef.current?.focus()}
+        />
+        <InputLogin
+          type="password"
+          label="Senha"
+          value={senha}
+          onChange={(newValue) => setSenha(newValue)}
+        />
+
+        {/* <label htmlFor="">
           <span>Email</span>
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" ? inputPasswordRef.current?.focus() : undefined
+            }
           />
         </label>
 
@@ -25,9 +65,10 @@ export const Login = () => {
           <input
             type="password"
             value={senha}
+            ref={inputPasswordRef}
             onChange={(e) => setSenha(e.target.value)}
           />
-        </label>
+        </label> */}
 
         <button type="button" onClick={handleEntrar}>
           Entrar
