289deb229f6ef02d6359f0c1af3974d5915c28a0
caiordev <caioreisdev@gmail.com>
2023-09-08 12:09:18 -0300

    feat: add json-server

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,34 +1,49 @@
-import { useCallback, useState } from "react";
-
-interface IListTarefa {
-  id: number;
-  title: string;
-  isCompleted: boolean;
-}
+import { useCallback, useEffect, useState } from "react";
+import { ApiException } from "../../shared/services/api/ApiException";
+import {
+  ITarefa,
+  TarefasService,
+} from "../../shared/services/api/tarefas/TarefasService";
 
 export const Dashboard = () => {
-  const [lista, setLista] = useState<IListTarefa[]>([]);
+  const [lista, setLista] = useState<ITarefa[]>([]);
+
+  useEffect(() => {
+    TarefasService.getAll().then((result) => {
+      if (result instanceof ApiException) {
+        alert(result.message);
+      } else {
+        setLista(result);
+      }
+    });
+  }, []);
 
   const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
-    useCallback((e) => {
-      if (e.key === "Enter") {
-        if (e.currentTarget.value.trim().length === 0) return;
-        const value = e.currentTarget.value;
-        e.currentTarget.value = "";
-        setLista((oldLista) => {
-          if (oldLista.some((listItem) => listItem.title === value))
-            return oldLista;
-          return [
-            ...oldLista,
-            {
-              id: oldLista.length,
-              title: value,
-              isCompleted: false,
-            },
-          ];
-        });
-      }
-    }, []);
+    useCallback(
+      (e) => {
+        if (e.key === "Enter") {
+          if (e.currentTarget.value.trim().length === 0) return;
+          const value = e.currentTarget.value;
+          e.currentTarget.value = "";
+
+          if (lista.some((listItem) => listItem.title === value)) return;
+          TarefasService.create({
+            title: value,
+            isCompleted: false,
+          }).then((result) => {
+            if (result instanceof ApiException) {
+              alert(result.message);
+            } else {
+              setLista((oldLista) => {
+                return [...oldLista, result];
+              });
+            }
+          });
+        }
+      },
+      [lista]
+    );
+
   return (
     <div>
       <p>Lista</p>
